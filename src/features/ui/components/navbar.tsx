8025b761171d03c8dfad2e523b47133920f1a74b
kasia-kusz <katarzyna.kuszx@gmail.com>
2023-09-07 21:53:56 +0200

    fix: link to navbar logo

--- src/features/ui/components/navbar.tsx
+++ src/features/ui/components/navbar.tsx
@@ -1,11 +1,14 @@
 import styles from "./navbar.module.scss";
 import logo from "../../../assets/logo.png";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <nav className={styles.container}>
       <div className={styles.logo}>
-        <img src={logo} alt="Logo inFakt" />
+        <Link title="Strona główna" to="/">
+          <img src={logo} alt="Logo inFakt" />
+        </Link>
       </div>
     </nav>
   );
